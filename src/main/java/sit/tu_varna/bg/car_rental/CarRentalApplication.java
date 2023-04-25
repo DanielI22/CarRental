61306ff74ee552d1eca6a6755030147b2213e431
Daniel Ivanov <80488757+DanielI22@users.noreply.github.com>
2023-04-25 14:54:13 +0300

    Init

--- src/main/java/sit/tu_varna/bg/car_rental/CarRentalApplication.java
+++ src/main/java/sit/tu_varna/bg/car_rental/CarRentalApplication.java
@@ -1,7 +1,10 @@
 package sit.tu_varna.bg.car_rental;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CarRentalApplication {
