d21485f36f663d037064a9751a5ebe5d67f40684
Ivaylo22 <ivi_petkow@abv.bg>
2023-05-13 13:49:17 +0300

    Added @CrossOrigin in RentalController

--- src/main/java/sit/tu_varna/bg/car_rental/controller/RentalController.java
+++ src/main/java/sit/tu_varna/bg/car_rental/controller/RentalController.java
@@ -1,6 +1,7 @@
 package sit.tu_varna.bg.car_rental.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import sit.tu_varna.bg.car_rental.model.RentalDto;
 import sit.tu_varna.bg.car_rental.service.CarService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class RentalController {
     private final CarService carService;
 
